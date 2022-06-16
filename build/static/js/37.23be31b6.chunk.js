f86654947053ba241e460dd4b0a2a575819e87da
root <root@ip-172-31-28-157.ec2.internal>
2022-06-16 05:21:55 +0000

    serversidechanges

--- build/static/js/37.23be31b6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[37],{1130:function(a,n){}}]);
-//# sourceMappingURL=37.23be31b6.chunk.js.map
--- build/static/js/38.417d8546.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[38],{1080:function(a,n){}}]);
-//# sourceMappingURL=38.417d8546.chunk.js.map
