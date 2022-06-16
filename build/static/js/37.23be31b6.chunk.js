5dbac1c86c9879da0308819b1c76537fac03a208
root <root@ip-172-31-28-157.ec2.internal>
2022-06-16 05:14:50 +0000

    serverupdate

---
+++ build/static/js/37.23be31b6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[37],{1130:function(a,n){}}]);
+//# sourceMappingURL=37.23be31b6.chunk.js.map
---
+++ build/static/js/38.417d8546.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[38],{1080:function(a,n){}}]);
+//# sourceMappingURL=38.417d8546.chunk.js.map
