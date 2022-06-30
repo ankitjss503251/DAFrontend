d455321351f727806e79566d50f3dfff9bea3635
neha-decrypt <neha.decrypt@gmail.com>
2022-06-30 12:16:42 +0000

    serversidechanges

--- build/static/js/37.e0d24f82.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[37],{1151:function(a,n){}}]);
-//# sourceMappingURL=37.e0d24f82.chunk.js.map
--- build/static/js/38.17b0099d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[38],{1101:function(a,n){}}]);
-//# sourceMappingURL=38.17b0099d.chunk.js.map
