eff13d2801752e7734495316b6a504ef28226488
neha-decrypt <neha.decrypt@gmail.com>
2022-07-05 15:30:12 +0530

    sidebar

--- admin/src/components/components/Sidebar.js
+++ admin/src/components/components/Sidebar.js
@@ -1,63 +1,80 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Dashboardsvg from '../SVG/dashboardsvg';
-import Formsvg from '../SVG/formsvg';
-import Tablesvg from '../SVG/tablesvg';
-import Performancesvg from '../SVG/performancesvg';
-import Collectionsvg from '../SVG/collectionsvg';
-import Nftsvg from '../SVG/nftsvg';
+import React from "react";
+import { NavLink } from "react-router-dom";
+// import Dashboardsvg from "../SVG/dashboardsvg";
+// import Formsvg from "../SVG/formsvg";
+// import Tablesvg from "../SVG/tablesvg";
+import Performancesvg from "../SVG/performancesvg";
+import Collectionsvg from "../SVG/collectionsvg";
+import Nftsvg from "../SVG/nftsvg";
 
 function Sidebar() {
-
-//   const [active, setActive] = useState('active');
+  //   const [active, setActive] = useState('active');
 
   return (
-        <nav id="sidebar">
-
-            <ul className="list-unstyled components text-uppercase">
-                <li>
-                    <NavLink to={"/"} className="text-decoration-none text-light" activeclassname="active-link">
-                        <Dashboardsvg /> Dashboard
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/form'} className="text-decoration-none text-light">
-                        <Formsvg /> Forms
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/table'} className="text-decoration-none text-light">
-                        <Tablesvg /> Tables
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/createcollection'} className="text-decoration-none text-light">
-                        <Collectionsvg /> Create Collection
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/createnfts'} className="text-decoration-none text-light">
-                        <Nftsvg /> Create NFTs
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/createbrands'} className="text-decoration-none text-light">
-                        <Performancesvg /> Create Brands
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/createcategories'} className="text-decoration-none text-light">
-                        <Performancesvg /> Create Categories
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/performance'} className="text-decoration-none text-light">
-                        <Performancesvg /> Performance
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-  )
+    <nav id="sidebar">
+      <ul className="list-unstyled components text-uppercase">
+        {
+          //   <li>
+          //     <NavLink
+          //       to={"/"}
+          //       className="text-decoration-none text-light"
+          //       activeclassname="active-link"
+          //     >
+          //  <Dashboardsvg /> Dashboard
+          //   </li>
+          //                 <li>
+          //                     <NavLink to={'/form'} className="text-decoration-none text-light">
+          //                         <Formsvg /> Forms
+          //                     </NavLink>
+          //                 </li>
+          //                 <li>
+          //                     <NavLink to={'/table'} className="text-decoration-none text-light">
+          //                         <Tablesvg /> Tables
+          //                     </NavLink>
+          //                 </li>
+        }
+        <li>
+          <NavLink
+            to={"/createcollection"}
+            className="text-decoration-none text-light"
+          >
+            <Collectionsvg /> Create Collection
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/createnfts"}
+            className="text-decoration-none text-light"
+          >
+            <Nftsvg /> Create NFTs
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/createbrands"}
+            className="text-decoration-none text-light"
+          >
+            <Performancesvg /> Create Brands
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/createcategories"}
+            className="text-decoration-none text-light"
+          >
+            <Performancesvg /> Create Categories
+          </NavLink>
+        </li>
+        {
+          // <li>
+          //     <NavLink to={'/performance'} className="text-decoration-none text-light">
+          //         <Performancesvg /> Performance
+          //     </NavLink>
+          // </li>
+        }
+      </ul>
+    </nav>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
