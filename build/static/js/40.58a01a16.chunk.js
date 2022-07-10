0ddb25a0f484327936de4895c8e438acb3428ad4
neha-decrypt <neha.decrypt@gmail.com>
2022-07-11 00:12:54 +0530

    fixed

---
+++ build/static/js/40.58a01a16.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[40],{1173:function(a,n){}}]);
+//# sourceMappingURL=40.58a01a16.chunk.js.map
---
+++ build/static/js/41.d2b30e66.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[41],{1123:function(a,n){}}]);
+//# sourceMappingURL=41.d2b30e66.chunk.js.map
