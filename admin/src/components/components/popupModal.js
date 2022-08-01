d2464df7a4dc7c75f83dcd4ae8d272b3fc4f5f95
Deepak Kumar <deepak.decrypt@gmail.com>
2022-08-01 14:57:22 +0530

    slider and arrow and image responsive isshue resolve

--- admin/src/components/components/popupModal.js
+++ admin/src/components/components/popupModal.js
@@ -3,10 +3,11 @@ import React from "react";
 const PopupModal = props => {
   return (
     <div className="popup-box">
-         <div className="box">
-        {props.content}
-      </div>
-    </div>
+    <span className="close-icon" onClick={props.handleClose}>x</span>
+     <div className="box">
+    {props.content}
+  </div>
+</div>
 
     
   );
--- src/components/pages/NFTDetails.js
+++ src/components/pages/NFTDetails.js
@@ -908,26 +908,13 @@ function NFTDetails() {
                 ""
               )}
               {NFTDetails && NFTDetails.fileType == "3D" ? (
-                // <Canvas shadows camera={{ position: [0, 0, 4], fov: 50 }}>
-                //   <ambientLight intensity={0.7} />
-                //   <Suspense fallback={null} scale={3.81}> 
-                //     <Model image={`http://${modalImage}`} angle={1.5} scale={3.81}/>
-                //   </Suspense>
-                //   <CameraControls />
-                // </Canvas>
-                <>
-                  <Canvas shadows camera={{ position: [0, 0, 4], fov: 50 }}>
-                    <ambientLight intensity={0.7} />
-                      <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
-                      <Suspense fallback={null}>
-                      <Model image={`http://${modalImage}`} />
-                        <Environment preset="city" />
-                        <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
-                      </Suspense>
-                    <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
-                  </Canvas>
-                  {/* <Picker /> */}
-                </>
+                <Canvas resolution={256} frames={Infinity} fog={customFog} near={1} far={1000}>
+                  <ambientLight />
+                  <Suspense fallback={null}> 
+                    <Model image={`http://${modalImage}`} angle={1.5}/>
+                  </Suspense>
+                  <CameraControls />
+                </Canvas>
               ) : (
                 ""
               )}
