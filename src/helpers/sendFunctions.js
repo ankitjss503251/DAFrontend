759490a2bac784a980cff94de0fb1fdea449d5f2
neha-decrypt <neha.decrypt@gmail.com>
2022-08-04 11:07:03 +0530

    fixed issues

--- src/helpers/sendFunctions.js
+++ src/helpers/sendFunctions.js
@@ -328,10 +328,7 @@ export const handleBuyNft = async (
   }
 
   NotificationManager.success("NFT Purchased Successfully");
-<<<<<<< HEAD
-=======
   return true
->>>>>>> 1cb852a8c860212de8ee431de79f8b8c5776a86b
   // slowRefresh(1000);
 };
 
